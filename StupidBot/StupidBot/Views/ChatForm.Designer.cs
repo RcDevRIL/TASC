3eb3a8396859e4e0c553a67639dcdb7032ca6a4a
Fabien Ladouce <ladouce.fabien@gmail.com>
2019-05-17 09:42:21 +0200

    StupidBot v0.1 :
    - Ajout appel chatForm dans MainForm
    - Ajout appel de l'ancienne MainForm dans chatForm

--- StupidBot/StupidBot/Views/ChatForm.Designer.cs
+++ StupidBot/StupidBot/Views/ChatForm.Designer.cs
@@ -31,8 +31,8 @@
             this.textBoxSend = new System.Windows.Forms.TextBox();
             this.btnSend = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
-            this.listViewChat = new System.Windows.Forms.ListView();
             this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.listViewChat = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // textBoxSend
@@ -41,6 +41,7 @@
             this.textBoxSend.Name = "textBoxSend";
             this.textBoxSend.Size = new System.Drawing.Size(281, 20);
             this.textBoxSend.TabIndex = 0;
+            this.textBoxSend.TextChanged += new System.EventHandler(this.textBoxSend_TextChanged);
             // 
             // btnSend
             // 
@@ -50,9 +51,11 @@
             this.btnSend.TabIndex = 1;
             this.btnSend.Text = "Envoyer";
             this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
             // btnExit
             // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnExit.Location = new System.Drawing.Point(13, 7);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 23);
@@ -61,14 +64,6 @@
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
-            // listViewChat
-            // 
-            this.listViewChat.Location = new System.Drawing.Point(13, 36);
-            this.listViewChat.Name = "listViewChat";
-            this.listViewChat.Size = new System.Drawing.Size(362, 328);
-            this.listViewChat.TabIndex = 3;
-            this.listViewChat.UseCompatibleStateImageBehavior = false;
-            // 
             // vScrollBar1
             // 
             this.vScrollBar1.Location = new System.Drawing.Point(378, 36);
@@ -76,13 +71,23 @@
             this.vScrollBar1.Size = new System.Drawing.Size(17, 328);
             this.vScrollBar1.TabIndex = 4;
             // 
+            // listViewChat
+            // 
+            this.listViewChat.Location = new System.Drawing.Point(13, 36);
+            this.listViewChat.Name = "listViewChat";
+            this.listViewChat.Size = new System.Drawing.Size(362, 328);
+            this.listViewChat.TabIndex = 5;
+            this.listViewChat.UseCompatibleStateImageBehavior = false;
+            // 
             // ChatForm
             // 
+            this.AcceptButton = this.btnSend;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
             this.ClientSize = new System.Drawing.Size(413, 416);
-            this.Controls.Add(this.vScrollBar1);
             this.Controls.Add(this.listViewChat);
+            this.Controls.Add(this.vScrollBar1);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.textBoxSend);
@@ -98,7 +103,7 @@
         private System.Windows.Forms.TextBox textBoxSend;
         private System.Windows.Forms.Button btnSend;
         private System.Windows.Forms.Button btnExit;
-        private System.Windows.Forms.ListView listViewChat;
         private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.ListView listViewChat;
     }
 }
--- StupidBot/StupidBot/Views/ContributionForm.Designer.cs
+++ StupidBot/StupidBot/Views/ContributionForm.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.labelReponse = new System.Windows.Forms.Label();
+            this.buttonValid = new System.Windows.Forms.Button();
+            this.buttonQuit = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
@@ -46,41 +46,44 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Ajout Question/Réponse";
             // 
-            // label2
+            // labelQuestion
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(31, 100);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.labelQuestion.AutoSize = true;
+            this.labelQuestion.Location = new System.Drawing.Point(31, 100);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(49, 13);
+            this.labelQuestion.TabIndex = 1;
+            this.labelQuestion.Text = "Question";
+            this.labelQuestion.Click += new System.EventHandler(this.label2_Click);
             // 
-            // label3
+            // labelReponse
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(31, 165);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.labelReponse.AutoSize = true;
+            this.labelReponse.Location = new System.Drawing.Point(31, 165);
+            this.labelReponse.Name = "labelReponse";
+            this.labelReponse.Size = new System.Drawing.Size(50, 13);
+            this.labelReponse.TabIndex = 2;
+            this.labelReponse.Text = "Réponse";
             // 
-            // button1
+            // buttonValid
             // 
-            this.button1.Location = new System.Drawing.Point(269, 296);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonValid.Location = new System.Drawing.Point(269, 296);
+            this.buttonValid.Name = "buttonValid";
+            this.buttonValid.Size = new System.Drawing.Size(75, 23);
+            this.buttonValid.TabIndex = 3;
+            this.buttonValid.Text = "Valider";
+            this.buttonValid.UseVisualStyleBackColor = true;
+            this.buttonValid.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // buttonQuit
             // 
-            this.button2.Location = new System.Drawing.Point(24, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonQuit.Location = new System.Drawing.Point(24, 12);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(75, 23);
+            this.buttonQuit.TabIndex = 4;
+            this.buttonQuit.Text = "Quitter";
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            this.buttonQuit.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
@@ -88,6 +91,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
@@ -103,10 +107,10 @@
             this.ClientSize = new System.Drawing.Size(365, 331);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonQuit);
+            this.Controls.Add(this.buttonValid);
+            this.Controls.Add(this.labelReponse);
+            this.Controls.Add(this.labelQuestion);
             this.Controls.Add(this.label1);
             this.Name = "ContributionForm";
             this.Text = "ContributionForm";
@@ -118,10 +122,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label labelQuestion;
+        private System.Windows.Forms.Label labelReponse;
+        private System.Windows.Forms.Button buttonValid;
+        private System.Windows.Forms.Button buttonQuit;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
     }
