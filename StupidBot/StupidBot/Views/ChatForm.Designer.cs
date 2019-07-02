e023da65ad828255211c023d03a063a763eeaf86
Romain <r.chevallier21@hotmail.fr>
2019-07-02 13:42:49 +0200

    fix merge remaining line

--- StupidBot/StupidBot/Views/ChatForm.Designer.cs
+++ StupidBot/StupidBot/Views/ChatForm.Designer.cs
@@ -102,7 +102,6 @@
             this.button1.Text = "Crash";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
->>>>>>> 9906c38... StupidBot
             // 
             // ChatForm
             // 
