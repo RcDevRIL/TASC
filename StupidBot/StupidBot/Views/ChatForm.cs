cec0baf5fdd0420521f90b64e5f71b5cb08268ca
Ladouce Fabien <ladouce.fabien@gmail.com>
2019-10-28 21:15:50 +0100

    Ajout tts StupidBoot

--- StupidBot/StupidBot/Views/ChatForm.cs
+++ StupidBot/StupidBot/Views/ChatForm.cs
@@ -8,16 +8,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech.Synthesis;
 
 namespace StupidBot.Views
 {
     public partial class ChatForm : Form
     {
         MainMenu mainForm;
+        SpeechSynthesizer tts = new SpeechSynthesizer();
         public ChatForm(MainMenu mainForm)
         {
             InitializeComponent();
             this.mainForm = mainForm;
+            tts.SetOutputToDefaultAudioDevice();
+            tts.Volume = 100;
+            tts.SpeakAsync("C'est l'heure de jouer");
         }
 
         private void btnExit_Click(object sender, EventArgs e)
@@ -29,6 +34,7 @@ namespace StupidBot.Views
         private void btnSend_Click(object sender, EventArgs e)
         {
             String textSend = textBoxSend.Text;
+            tts.SpeakAsync(textSend);
             listViewChat.Items.Add(textSend);
             textBoxSend.Clear();
             Log.StupidLogger.Info("Vous avez bien envoyer le message !! youpiiiii");
